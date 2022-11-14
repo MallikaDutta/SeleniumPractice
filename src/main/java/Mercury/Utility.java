08bcf360739ba2fb4eb5902c956349e35c20d3e3
Mallika Dutta <mallika.duta39@gmail.com>
2022-11-14 10:33:36 +0530

    first commit

---
+++ src/main/java/Mercury/Utility.java
@@ -0,0 +1,27 @@
+package Mercury;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Utility {
+	
+public static void waitForElement(WebElement element, Duration waitTime, WebDriver driver) {
+		
+		WebDriverWait wait = new WebDriverWait(driver, waitTime);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	
+public static void waitTill(int waitTime) {
+		try {
+			Thread.sleep(waitTime);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
